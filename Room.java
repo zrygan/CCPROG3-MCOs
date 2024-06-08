728a7e055f4cb8d4bd36be666087bc27c732d72a
zrygan <zr.gntn@gmail.com>
2024-06-08 12:32:20 +0800

    Added check-in and -out functions

--- Room.java
+++ Room.java
@@ -4,16 +4,19 @@
  * @params:
  *  - name      : String  : the name of the room, must be different for each room
  *  - basePrice : int     : the base price of the room, always 1299.00 but can be changed using the manage hotel method
+ *  - isBooked  : Boolean : determines if the room is currently booked 
  */
 public class Room {
     // Variables
     private String name;
     private double basePrice;
+    private boolean isBooked;
 
     // Constructor
     public Room(String name){
         this.name = name;
         this.basePrice = 1299.0;
+        this.isBooked = false; // init the isBooked as false
     }
 
     // Setters
@@ -25,6 +28,10 @@ public class Room {
         this.basePrice = basePrice;
     }
 
+    public void setIsBooked(boolean isBooked){
+        this.isBooked = isBooked;
+    }
+
     // Getters
     public String getName(){
         return name;
@@ -34,4 +41,31 @@ public class Room {
         return basePrice;
     }
 
+    public boolean getIsBooked(){
+        return isBooked;
+    }
+
+    /* checkIn
+     * function that sets the booking checker of the room to true
+     * 
+     * @params:
+     *  - none
+     * 
+     * @author: Zhean Ganiuen
+     */
+    public void checkIn(){
+        this.isBooked = true;
+    }
+
+    /* checkOut
+     * function that sets the booking checker of the room to false
+     * 
+     * @params:
+     *  - none
+     * 
+     * @author: Zhean Ganiuen
+     */
+    public void checkOut(){
+        this.isBooked = false;
+    }
 }
