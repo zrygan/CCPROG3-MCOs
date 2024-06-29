074a27b1b330323463291ecddb6995f4e84acfb5
zrygan <zr.gntn@gmail.com>
2024-06-30 04:16:22 +0800

    Added design specs for Main_GUI

--- src/HotelReservationSystem/Main_GUI.java
+++ src/HotelReservationSystem/Main_GUI.java
@@ -2,6 +2,23 @@ package HotelReservationSystem;
 
 import javax.swing.*;
 
+/*
+ * ========== DESIGN SPECS for Main_GUI ==========
+ * 
+ * Dimensions:
+ *      W   450 (px)
+ *      H   500 (px)
+ * 
+ * Fonts:
+ *      FONT_NAME   TTF_FILENAME    GOOGLE_FONTS_LINK
+ *      ...
+ * 
+ * Colors: ADOBE_COLORS_LINK
+ *      COLOR_NAME  HEX_CODE    DESC (is it for text, background, highlight, button?)
+ *      ...
+ * 
+ */
+
 public class Main_GUI extends JFrame {
 
     public Main_GUI() {
