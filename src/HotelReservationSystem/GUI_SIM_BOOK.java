d226153751aaf63d7c69cbc87d682836c0efa056
jazjimenez <jaztin_jacob_jimenez@dlsu.edu.ph>
2024-07-23 14:09:28 +0800

    updated SIM_BOOK GUI

--- src/HotelReservationSystem/GUI_SIM_BOOK.java
+++ src/HotelReservationSystem/GUI_SIM_BOOK.java
@@ -38,7 +38,7 @@ public class GUI_SIM_BOOK extends GUI {
         setTitle("Hotel Reservation System: Simulate Booking");
         setSize(window_width, window_height); // width = 500 (px) height = 750 (px)
         GUI_CONFIG_WINDOW_CLOSE();
-        setLayout(getLayout());
+        setLayout(null);
         setResizable(false);
 
         ArrayList<JPanel> panels = Assets.ASSET_ADD_PANELS(6);
@@ -63,8 +63,6 @@ public class GUI_SIM_BOOK extends GUI {
         JTextField guest_name = Assets.ASSET_TEXT_FIELD("Guest Name");
         guest_name.setPreferredSize(new Dimension(300, 45));
         panels.get(1).add(guest_name);
-        
-        panels.add(Assets.ASSET_SEPARATOR(window_width));
 
         // [MENU BAR] Hotel Name Menu Bar
         JMenuBar hotel_name = Assets.ASSET_MENU_BAR(hrs, "Hotel Name");
@@ -75,7 +73,7 @@ public class GUI_SIM_BOOK extends GUI {
          * contains: In-Day text box
          */
         panels.get(2).setBounds( 0, 210, window_width/2, 100);        // panels[2] = mid-left panel
-        panels.get(2).setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 50));
+        panels.get(2).setLayout(new FlowLayout(FlowLayout.RIGHT, 30, 30));
 
         // [TEXT BOX] In-Day
         JTextField in_day = Assets.ASSET_TEXT_FIELD("In-Day");
@@ -86,12 +84,48 @@ public class GUI_SIM_BOOK extends GUI {
          * contains: Out-Day text box
          */
         panels.get(3).setBounds(window_width/2, 210, window_width, 100);
-        panels.get(3).setLayout(new FlowLayout(FlowLayout.LEFT, 10, 50));
+        panels.get(3).setLayout(new FlowLayout(FlowLayout.LEFT, 30, 30));
 
         // [TEXT BOX] In-Day
         JTextField out_day = Assets.ASSET_TEXT_FIELD("In-Day");
         out_day.setPreferredSize(new Dimension(75, 45));
-        panels.get(2).add(out_day);
+        panels.get(3).add(out_day);
+
+        /* code of MID-LOW PANEL (panel : 4)
+         * contains: Room type Menu Bar, Discount Code text field
+         */
+        panels.get(4).setBounds( 0, 310, window_width, 130);
+        panels.get(4).setLayout(new FlowLayout(FlowLayout.CENTER, 20, 10));
+
+        // [MENU BAR] Room Type
+        JMenuBar room_type = Assets.ASSET_MENU_BAR(hrs, "Room Type");
+        room_type.setPreferredSize(new Dimension(300, 45));
+        panels.get(4).add(room_type);
+
+        // [TEXT BOX] Discount Code
+        JTextField discount_code = Assets.ASSET_TEXT_FIELD("Discount Code");
+        discount_code.setPreferredSize(new Dimension(300, 45));
+        panels.get(4).add(discount_code);
+
+        /* code of LOW PANEL (panel : 5)
+         * contains: Book Room Button
+         */
+        panels.get(5).setBounds( 0, 440, window_width, 310);
+        panels.get(5).setLayout(new FlowLayout(FlowLayout.CENTER, 20, 30));
+
+        // [BUTTON] Book Room
+        JButton book_room = Assets.ASSET_ACCENT_BUTTON("Book Room");
+        //book_room.addActionListener(e -> {
+            //String guest_name_str = guest_name.getText();
+            //String hotel_name_str = ((JMenu) hotel_name.getSelectedItem()).getText();
+            //int in_day_int = getInput(in_day);
+            //int out_day_int = getInput(out_day);
+            //String room_type_str = ((JMenu) room_type.getSelectedItem()).getText();
+            //String discount_code_str = discount_code.getText();
+            //hrs.simBooking(sc, guest_name_str, hotel_name_str, in_day_int, out_day_int, room_type_str, discount_code_str);
+        //});
+        book_room.setPreferredSize(new Dimension(300, 100));
+        panels.get(5).add(book_room);
 
         for (JPanel panel : panels){
             panel.setBackground(Colors.getBlack());
